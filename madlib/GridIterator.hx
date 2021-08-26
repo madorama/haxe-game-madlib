cf382232017e3c19e6ad12f3c60d10f3928b9199
madorama <mamunabo@hotmail.co.jp>
2021-08-26 11:41:46 +0900

    🚑 Add: GridIterator

---
+++ madlib/GridIterator.hx
@@ -0,0 +1,32 @@
+package madlib;
+
+class GridIterator {
+    final width = 0;
+    final height = 0;
+    var i: Int = 0;
+
+    public inline function new(w: Int, h: Int) {
+        width = w;
+        height = h;
+    }
+
+    public inline function hasNext() {
+        return i < gridWidth * gridHeight;
+    }
+
+    public inline function next() {
+        return new GridIteratorObject(i++, gridWidth);
+    }
+}
+
+class GridIteratorObject {
+    public var index(default, null): Int;
+    public var x(default, null): Int;
+    public var y(default, null): Int;
+
+    public inline function new(index: Int, gridWidth: Int) {
+        this.index = index;
+        this.x = index % gridWidth;
+        this.y = Std.int(index / gridWidth);
+    }
+}
