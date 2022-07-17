a85fb3fac58eb48220d9daca3d688012618753c6
madorama <madorama999@gmail.com>
2022-07-17 18:32:06 +0900

    💥 Change: Debug functions return value

--- madlib/Debug.hx
+++ madlib/Debug.hx
@@ -6,30 +6,34 @@ import hx.strings.ansi.Ansi;
 
 class Debug {
     @:nullSafety(Off)
-    public inline static function log(value: Dynamic, ?pos: PosInfos) {
+    public inline static function log<T>(value: T, ?pos: PosInfos): T {
         #if debug
         trace('${Ansi.fg(WHITE)}[Log] ${value}${Ansi.fg(DEFAULT)}');
         #end
+        return value;
     }
 
     @:nullSafety(Off)
-    public inline static function warn(value: Dynamic, ?pos: PosInfos) {
+    public inline static function warn<T>(value: T, ?pos: PosInfos): T {
         #if debug
         trace('${Ansi.fg(YELLOW)}[Warn] ${value}${Ansi.fg(DEFAULT)}');
         #end
+        return value;
     }
 
     @:nullSafety(Off)
-    public inline static function error(value: Dynamic, ?pos: PosInfos) {
+    public inline static function error<T>(value: T, ?pos: PosInfos): T {
         trace('${Ansi.fg(RED)}[Error] ${value}${Ansi.fg(DEFAULT)}');
+        return value;
     }
 
     @:nullSafety(Off)
-    public inline static function errorLog(value: Dynamic, ?pos: PosInfos) {
+    public inline static function errorLog(value: T, ?pos: PosInfos): T {
         #if hl
         hl.UI.dialog("Error log", '${value}\n${pos.fileName}:${pos.lineNumber}', EnumFlags.ofInt(2));
         #else
         error(value, pos);
         #end
+        return value;
     }
 }
