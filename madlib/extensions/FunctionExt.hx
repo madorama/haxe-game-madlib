129b7a48ba270de3b0379eec0bdc48038aeb88de
madorama <madorama999@gmail.com>
2023-06-19 20:52:06 +0900

    💥 Change: Overloaded function extensions

--- madlib/extensions/FunctionExt.hx
+++ madlib/extensions/FunctionExt.hx
@@ -3,108 +3,110 @@ package madlib.extensions;
 class FunctionExt {
     public inline static function identity<T>(x: T): T
         return x;
-}
 
-class Function0Ext {
-    public inline static function toEffect<T>(func: Void -> T): Void -> Void
-        return () -> func();
-}
+    extern overload public inline static function pipe<A, B>(value: A, f1: A -> B): B
+        return f1(value);
 
-class Function1Ext {
-    @:nullSafety(Off)
-    public inline static function lazy<A, R>(func: A -> R, v: A): Void -> R {
-        var r: Null<R> = null;
-        return () -> if(r == null) r = func(v) else r;
-    }
+    extern overload public inline static function pipe<A, B, C>(value: A, f1: A -> B, f2: B -> C): C
+        return f2(f1(value));
 
-    public inline static function toEffect<A, R>(func: A -> R): A -> Void
-        return (a) -> func(a);
-}
+    extern overload public inline static function pipe<A, B, C, D>(value: A, f1: A -> B, f2: B -> C, f3: C -> D): D
+        return f3(f2(f1(value)));
 
-class Function2Ext {
-    public inline static function curry<A, B, R>(func: A -> B -> R): A -> (B -> R)
+    extern overload public inline static function pipe<A, B, C, D, E>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E): E
+        return f4(f3(f2(f1(value))));
+
+    extern overload public inline static function pipe<A, B, C, D, E, F>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E, f5: E -> F): F
+        return f5(f4(f3(f2(f1(value)))));
+
+    extern overload public inline static function pipe<A, B, C, D, E, F, G>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E, f5: E -> F,
+            f6: F -> G): G
+        return f6(f5(f4(f3(f2(f1(value))))));
+
+    extern overload public inline static function pipe<A, B, C, D, E, F, G, H>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E, f5: E -> F,
+            f6: F -> G, f7: G -> H): H
+        return f7(f6(f5(f4(f3(f2(f1(value)))))));
+
+    extern overload public inline static function pipe<A, B, C, D, E, F, G, H, I>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E, f5: E -> F,
+            f6: F -> G, f7: G -> H, f8: H -> I): I
+        return f8(f7(f6(f5(f4(f3(f2(f1(value))))))));
+
+    extern overload public inline static function pipe<A, B, C, D, E, F, G, H, I, J>(value: A, f1: A -> B, f2: B -> C, f3: C -> D, f4: D -> E, f5: E -> F,
+            f6: F -> G, f7: G -> H, f8: H -> I, f9: I -> J): J
+        return f9(f8(f7(f6(f5(f4(f3(f2(f1(value)))))))));
+
+    extern overload public inline static function curry<A, B, C>(func: A -> B -> C): A -> (B -> C)
         return a -> b -> func(a, b);
 
-    public inline static function uncurry<A, B, R>(func: A -> (B -> R)): A -> B -> R
+    extern overload public inline static function curry<A, B, C, D>(func: A -> B -> C -> D): A -> (B -> (C -> D))
+        return a -> b -> c -> func(a, b, c);
+
+    extern overload public inline static function curry<A, B, C, D, E>(func: A -> B -> C -> D -> E): A -> (B -> (C -> (D -> E)))
+        return a -> b -> c -> d -> func(a, b, c, d);
+
+    extern overload public inline static function curry<A, B, C, D, E, F>(func: A -> B -> C -> D -> E -> F): A -> (B -> (C -> (D -> (E -> F))))
+        return a -> b -> c -> d -> e -> func(a, b, c, d, e);
+
+    extern overload public inline static function uncurry<A, B, C>(func: A -> (B -> C)): A -> B -> C
         return (a, b) -> func(a)(b);
 
-    public inline static function flip<A, B, R>(func: A -> B -> R): B -> A -> R
-        return (b, a) -> func(a, b);
+    extern overload public inline static function uncurry<A, B, C, D>(func: A -> (B -> (C -> D))): A -> B -> C -> D
+        return (a, b, c) -> func(a)(b)(c);
+
+    extern overload public inline static function uncurry<A, B, C, D, E>(func: A -> (B -> (C -> (D -> E)))): A -> B -> C -> D -> E
+        return (a, b, c, d) -> func(a)(b)(c)(d);
+
+    extern overload public inline static function uncurry<A, B, C, D, E, F>(func: A -> (B -> (C -> (D -> (E -> F))))): A -> B -> C -> D -> E -> F
+        return (a, b, c, d, e) -> func(a)(b)(c)(d)(e);
+
+    extern overload public inline static function toEffect<T>(func: Void -> T): Void -> Void
+        return () -> func();
+
+    extern overload public inline static function toEffect<A, B>(func: A -> B): A -> Void
+        return (a) -> func(a);
+
+    extern overload public inline static function toEffect<A, B, C>(func: A -> B -> C): A -> B -> Void
+        return (a, b) -> func(a, b);
+
+    extern overload public inline static function toEffect<A, B, C, D>(func: A -> B -> C -> D): A -> B -> C -> Void
+        return (a, b, c) -> func(a, b, c);
+
+    extern overload public inline static function toEffect<A, B, C, D, E>(func: A -> B -> C -> D -> E): A -> B -> C -> D -> Void
+        return (a, b, c, d) -> func(a, b, c, d);
+
+    extern overload public inline static function toEffect<A, B, C, D, E, F>(func: A -> B -> C -> D -> E -> F): A -> B -> C -> D -> E -> Void
+        return (a, b, c, d, e) -> func(a, b, c, d, e);
+
+    public inline static function flip<A, B, C>(f: A -> (B -> C)): B -> (A -> C)
+        return a -> b -> f(b)(a);
 
     @:nullSafety(Off)
-    public inline static function lazy<A, B, R>(func: A -> B -> R, a: A, b: B): Void -> R {
-        var r: Null<R> = null;
+    extern overload public inline static function lazy<A, B>(func: A -> B, a: A): Void -> B {
+        var r: Null<B> = null;
+        return () -> if(r == null) r = func(a) else r;
+    }
+
+    @:nullSafety(Off)
+    extern overload public inline static function lazy<A, B, C>(func: A -> B -> C, a: A, b: B): Void -> C {
+        var r: Null<C> = null;
         return () -> if(r == null) r = func(a, b) else r;
     }
 
-    public inline static function toEffect<A, B, R>(func: A -> B -> R): A -> B -> Void
-        return (a, b) -> func(a, b);
-}
-
-class Function3Ext {
-    public inline static function curry<A, B, C, R>(func: A -> B -> C -> R): A -> (B -> (C -> R))
-        return a -> b -> c -> func(a, b, c);
-
-    public inline static function uncurry<A, B, C, R>(func: A -> (B -> (C -> R))): A -> B -> C -> R
-        return (a, b, c) -> func(a)(b)(c);
-
     @:nullSafety(Off)
-    public inline static function lazy<A, B, C, R>(func: A -> B -> C -> R, a: A, b: B, c: C): Void -> R {
-        var r: Null<R> = null;
+    extern overload public inline static function lazy<A, B, C, D>(func: A -> B -> C -> D, a: A, b: B, c: C): Void -> D {
+        var r: Null<D> = null;
         return () -> if(r == null) r = func(a, b, c) else r;
     }
 
-    public inline static function toEffect<A, B, C, R>(func: A -> B -> C -> R): A -> B -> C -> Void
-        return (a, b, c) -> func(a, b, c);
-}
-
-class Function4Ext {
-    public inline static function curry<A, B, C, D, R>(func: A -> B -> C -> D -> R): A -> (B -> (C -> (D -> R)))
-        return a -> b -> c -> d -> func(a, b, c, d);
-
-    public inline static function uncurry<A, B, C, D, R>(func: A -> (B -> (C -> (D -> R)))): A -> B -> C -> D -> R
-        return (a, b, c, d) -> func(a)(b)(c)(d);
-
     @:nullSafety(Off)
-    public inline static function lazy<A, B, C, D, R>(func: A -> B -> C -> D -> R, a: A, b: B, c: C, d: D): Void -> R {
-        var r: Null<R> = null;
+    extern overload public inline static function lazy<A, B, C, D, E>(func: A -> B -> C -> D -> E, a: A, b: B, c: C, d: D): Void -> E {
+        var r: Null<E> = null;
         return () -> if(r == null) r = func(a, b, c, d) else r;
     }
 
-    public inline static function toEffect<A, B, C, D, R>(func: A -> B -> C -> D -> R): A -> B -> C -> D -> Void
-        return (a, b, c, d) -> func(a, b, c, d);
-}
-
-class Function5Ext {
-    public inline static function curry<A, B, C, D, E, R>(func: A -> B -> C -> D -> E -> R): A -> (B -> (C -> (D -> (E -> R))))
-        return a -> b -> c -> d -> e -> func(a, b, c, d, e);
-
-    public inline static function uncurry<A, B, C, D, E, R>(func: A -> (B -> (C -> (D -> (E -> R))))): A -> B -> C -> D -> E -> R
-        return (a, b, c, d, e) -> func(a)(b)(c)(d)(e);
-
     @:nullSafety(Off)
-    public inline static function lazy<A, B, C, D, E, R>(func: A -> B -> C -> D -> E -> R, a: A, b: B, c: C, d: D, e: E): Void -> R {
-        var r: Null<R> = null;
+    extern overload public inline static function lazy<A, B, C, D, E, F>(func: A -> B -> C -> D -> E -> F, a: A, b: B, c: C, d: D, e: E): Void -> F {
+        var r: Null<F> = null;
         return () -> if(r == null) r = func(a, b, c, d, e) else r;
     }
-
-    public inline static function toEffect<A, B, C, D, E, R>(func: A -> B -> C -> D -> E -> R): A -> B -> C -> D -> E -> Void
-        return (a, b, c, d, e) -> func(a, b, c, d, e);
-}
-
-class Function6Ext {
-    public inline static function curry<A, B, C, D, E, F, R>(func: A -> B -> C -> D -> E -> F -> R): A -> (B -> (C -> (D -> (E -> (F -> R)))))
-        return a -> b -> c -> d -> e -> f -> func(a, b, c, d, e, f);
-
-    public inline static function uncurry<A, B, C, D, E, F, R>(func: A -> (B -> (C -> (D -> (E -> (F -> R)))))): A -> B -> C -> D -> E -> F -> R
-        return (a, b, c, d, e, f) -> func(a)(b)(c)(d)(e)(f);
-
-    @:nullSafety(Off)
-    public inline static function lazy<A, B, C, D, E, F, R>(func: A -> B -> C -> D -> E -> F -> R, a: A, b: B, c: C, d: D, e: E, f: F): Void -> R {
-        var r: Null<R> = null;
-        return () -> if(r == null) r = func(a, b, c, d, e, f) else r;
-    }
-
-    public inline static function toEffect<A, B, C, D, E, F, R>(func: A -> B -> C -> D -> E -> F -> R): A -> B -> C -> D -> E -> F -> Void
-        return (a, b, c, d, e, f) -> func(a, b, c, d, e, f);
 }
