54ebeb915c3168142a966949df926ecc0de78286
madorama <madorama999@gmail.com>
2024-01-06 19:25:28 +0900

    ⚡️ Change: Improve WalkersAlias initialize

--- madlib/WalkersAlias.hx
+++ madlib/WalkersAlias.hx
@@ -5,104 +5,85 @@ import haxe.exceptions.ArgumentException;
 using madlib.extensions.ArrayExt;
 using madlib.extensions.MapExt;
 
-typedef WalkersAliasItem<T> = {
+@:structInit
+@:allow(madlib.WalkersAlias)
+class WalkersAliasItem<T> {
     final item: T;
     final weight: Float;
+
+    public inline function new(item: T, weight: Float) {
+        this.item = item;
+        this.weight = weight;
+    }
 }
 
 class WalkersAlias<T> {
     var normalized: Array<Float> = [];
     var aliases: Array<Int> = [];
-    var items: Array<T> = [];
-    var weights: Array<Float> = [];
+    var items: Array<WalkersAliasItem<T>>;
 
     public function new(items: Array<WalkersAliasItem<T>>) {
         if(items.length == 0)
             throw new ArgumentException("items must be an array with at least one element.");
-        for(x in items) {
-            this.items.push(x.item);
-            weights.push(if(x.weight > 0.0) x.weight else 0);
-        }
+        this.items = items.copy();
         refresh();
     }
 
-    inline function getWeight(index: Int): Option<Float>
-        return weights.getOption(index);
-
     public function refresh() {
-        final length = weights.length;
+        final length = items.length;
+
         normalized = [];
-        aliases = [];
-        final indexes = [];
+        normalized.resize(length);
+        aliases.resize(length);
+        final indexes: Array<Int> = aliases.copy();
 
         var totalWeight = 0.0;
-        for(i in 0...length) {
-            switch getWeight(i) {
-                case Some(weight):
-                    if(weight > 0.0)
-                        totalWeight += weight;
-                case None:
-            }
+        for(i => item in items) {
+            final w = item.weight;
+            if(w > 0.0) totalWeight += w;
         }
 
         final normalizeRatio = length / totalWeight;
-        var left = -1;
-        var right = length;
-        for(i in 0...length) {
+        var left = 0;
+        var right = length - 1;
+        for(i => item in items) {
             aliases[i] = i;
-            switch getWeight(i) {
-                case None:
-                case Some(weight):
-                    var weight = weight;
-                    weight *= if(weight > 0.0) normalizeRatio else 0;
-                    normalized[i] = weight;
-
-                    if(weight < 1.0)
-                        indexes[++left] = i;
-                    else
-                        indexes[--right] = i;
-            }
+            final w = item.weight;
+            normalized[i] = if(item.weight > 0.0) item.weight * normalizeRatio else 0;
+            if(normalized[i] < 1.0)
+                indexes[left++] = i;
+            else
+                indexes[right--] = i;
         }
 
-        if(left >= 0 && right < length) {
-            left = 0;
-            while(left < length && right < length) {
-                final leftIndex = indexes[left];
-                final rightIndex = indexes[right];
-                aliases[leftIndex] = rightIndex;
-                final leftWeight = normalized[leftIndex];
-                final rightWeight = normalized[rightIndex] + leftWeight - 1.0;
-                normalized[rightIndex] = rightWeight;
-                if(rightWeight < 1.0)
-                    right += 1;
-                left += 1;
+        while(left > 0 && right < length - 1) {
+            final leftIndex = indexes[left - 1];
+            final rightIndex = indexes[right + 1];
+            aliases[leftIndex] = rightIndex;
+            normalized[rightIndex] -= 1 - normalized[leftIndex];
+            if(normalized[rightIndex] < 1) {
+                indexes[left - 1] = rightIndex;
+                right += 1;
+            } else {
+                left -= 1;
             }
         }
     }
 
-    public function randomIndex(?randomGenerator: Random): Int {
-        var length = weights.length;
-
-        if(length == 0)
-            return -1;
-
+    inline function randomIndex(?randomGenerator: Random): Int {
         final gen = randomGenerator ?? Random.gen;
-        final random = gen.random() * length;
-        var index = Math.floor(random);
-        final isOutbounds = index >= length;
-        final weight = if(isOutbounds) 1.0 else random - index;
-        if(isOutbounds)
-            index = length - 1;
-
-        if(normalized[index] <= weight)
-            index = aliases[index];
-
-        return index;
+        final random = gen.uniform(0, items.length);
+        final index = Std.int(random);
+        return if(normalized[index] <= random - index) {
+            aliases[index];
+        } else {
+            index;
+        }
     }
 
     @:nullSafety(Off)
     public inline function random(?randomGenerator: Random): T {
         final index = randomIndex(randomGenerator);
-        return items[index];
+        return items[index].item;
     }
 }
